46a18da148b334e80a77771841a1dc4a6c084a64
Jason Chase <jasonpchase@gmail.com>
2015-04-05 12:08:54 -0400

    Convert auto tests to use Cordova plugin test framework

--- tests/tests.js
+++ tests/tests.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-registerAutoTests("chrome.storage", function() {
+exports.defineAutoTests = function() {
   'use strict';
 
   var storage_local_items = {};
@@ -572,5 +572,4 @@ registerAutoTests("chrome.storage", function() {
       });
     });
   });
-});
-
+};
